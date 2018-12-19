dd4494367591d28b3cf0d08409ddc21568ed9349
nightflyza <nightfly@email.ua>
2018-12-19 12:21:02 +0200

    removed old build scripts

--- nas_preconf/services/packages_pack120.sh
+++ nas_preconf/services/packages_pack120.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PLATFORM="NAS_111_64"
+PLATFORM="NAS_120_64"
 PKGPATH="/home/"${PLATFORM}
 
 mkdir ${PKGPATH}
@@ -13,11 +13,12 @@ pkg install -y  expat2
 pkg install -y  bash
 pkg install -y  nginx
 pkg install -y  softflowd
-pkg install -y  vim-lite
+pkg install -y  vim-tiny
 pkg install -y  nano
 pkg install -y  mtr-nox11
 pkg install -y  bandwidthd
 pkg install -y  trafshow
+#elinks may not be accessible
 pkg install -y  elinks
 pkg install -y  net-snmp
 pkg install -y  php56
